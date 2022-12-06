6b7357610b043130cae49510ccc9a4a1e2446c1b
Jacob Webber <jacobwwebber@gmail.com>
2022-12-05 21:03:53 -0500

    introduce queries
    introduce backend

---
+++ backend/index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const db = require('./server/queries')
+const bodyParser = require('body-parser');
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+express()
+  .use(bodyParser.urlencoded({extended: false}))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/about'))
+  .get('/menu', (req, res) => res.render('pages/menu'))
+  .get('/order', (req, res) => res.render('pages/order'))
+  .get('/charity', (req, res) => res.render('pages/charity'))
+  .get('/faq', (req, res) => res.render('pages/faq'))
+  .get('/admin', (req, res) => db.getOrdersAdmin(req, res))
+  .get('/admintab', (req, res) => db.getGroupedOrders(req, res, 'admintab'))
+  .get('/jumbotron', (req, res) => db.getGroupedOrders(req, res, 'jumbotron'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM drink_orders2 WHERE is_done != true ORDER BY date DESC');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .post('/send', (req, res) => {
+    db.createOrder(req, res);
+    console.log(req);
+    console.log(db);
+  })
+  .post('/updatePaid', (req, res) => db.updatePaid(req, res))
+  .post('/updateDone', (req, res) => db.updateDone(req, res))
+  .post('/closeBarTab', (req, res) => db.closeBarTab(req, res))
+
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ backend/queries.js
@@ -0,0 +1,129 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    multipleStatements: true
+  });
+
+const getOrders = (request, response) => {
+    pool.query('SELECT * FROM drink_orders2 WHERE is_done != true ORDER BY date DESC', (error, result) => {
+        if (error) {
+            console.error(error);
+            result.send("Error " + error)
+        }
+        response.status(200).json(result.rows)
+        const results = { 'results': (result) ? result.rows : null};
+        response.render('pages/db', results );
+    })
+  }
+
+  /** Create a drink order.
+   * params: name, amount, cost, drinkTitle, comments */
+const createOrder = (request, response) => {
+  // field from form bots - do nothing
+  if(request.body.email || request.body.comments.length > 500) return;
+
+  const { name, amount, drinkCost, drinkTitle, customDrinkTitle, comments} = request.body
+  let text = `INSERT INTO drink_orders2(name, amount, cost, drink_title, date, comments, is_paid, is_done) 
+              VALUES ($1, $2, $3, $4, NOW(), $5, false, false) 
+              RETURNING id`;
+
+  pool.query(text, [name.trim(), parseInt(amount), parseInt(amount * drinkCost), drinkTitle || customDrinkTitle.trim(), comments.trim()], (error, result) => {
+    if (error) {
+      console.log('error')
+      throw error;
+    }
+    
+    // append result ID to url for searching on order page
+    var id = result.rows[0].id;
+    var url = result && result.rows.length > 0 ? '/db#order-' + id : '/db';
+    response.redirect(url);
+  })
+}
+
+const updatePaid = (request, response) => {
+    const { orderId, paidStatus } = request.body
+    var isPaid = paidStatus ? 1 : 0;
+    pool.query(
+        'UPDATE drink_orders2 SET is_paid = $1 WHERE id = $2',
+        [isPaid, orderId],
+        (error, results) => {
+        if (error) {
+            throw error;
+        }
+        var msg = `updated order ID: ${orderId} to ${isPaid}`;
+        console.log(msg)
+        response.status(200).send(msg);
+        }
+    )
+}
+
+const updateDone = (request, response) => {
+  const { orderId, doneStatus } = request.body
+  var isDone = doneStatus ? 1 : 0;
+  pool.query(
+      'UPDATE drink_orders2 SET is_done = $1 WHERE id = $2',
+      [isDone, orderId],
+      (error, results) => {
+      if (error) {
+          throw error;
+      }
+      var msg = `updated order ID: ${orderId} to ${isDone}`;
+      console.log(msg)
+      response.status(200).send(msg);
+      }
+  )
+}
+
+const closeBarTab = (request, response) => {
+  const { barTabName } = request.body
+
+  pool.query(
+      `UPDATE drink_orders2 SET is_paid = true WHERE UPPER(name) = UPPER($1) and is_paid = false`,
+      [barTabName],
+      (error, results) => {
+      if (error) {
+          throw error;
+      }
+      var msg = `paid bar tab for ${barTabName}}`;
+      console.log(msg)
+      response.status(200).send(msg);
+      }
+  )
+}
+
+
+const getOrdersAdmin = async (request, response) => {
+  pool.query('SELECT * FROM drink_orders2 ORDER BY date DESC', (error, result) => {
+      if (error) {
+          console.error(error);
+          result.send("Error " + error)
+      }
+      const results = { 'results': (result) ? result.rows : null};
+      response.render('pages/admin', results );
+  })
+}
+
+const getGroupedOrders = async (request, response, page) => {
+  pool.query('SELECT * FROM orders_by_name ORDER BY total_cost DESC', (error, result) => {
+      if (error) {
+          console.error(error);
+          response.send("Error " + error)
+      }
+      const results = { 'results': (result) ? result.rows : null};
+      response.render(`pages/${page}`, results );
+  })
+}
+
+  module.exports = {
+      getOrders,
+      createOrder,
+      updatePaid,
+      updateDone,
+      closeBarTab,
+      getOrdersAdmin,
+      getGroupedOrders
+  }
